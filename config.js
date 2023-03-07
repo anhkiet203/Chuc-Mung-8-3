f32d17c102cc16d531cf9fb9fbab429df5c9b03a
anhkiet203 <125114567+anhkiet203@users.noreply.github.com>
2023-03-07 20:12:47 +0700

    Add files via upload
    creat thiep

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Cậu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi Cậu "Thùy Trang';
+const contentCard = 'Chúc Cậu 8/3 vui vẻ và ngày xinh đẹp hơn, tớ hong có gì ngoài tấm lòng này muốn giành gửi tới cậu, tuy là mới quen nhau không được bao lâu nhưng tớ muốn gửi tới cậu những lời tốt đẹp nhất , mong cậu tìm được một nữa còn lại của cuộc đời mình.';
+// Hãy theo giỏi tui facebook " Dinh Kiet" bạn sẽ bất ngờ vì tôi đó!!!!
+const giftImage = 'Anh11.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
